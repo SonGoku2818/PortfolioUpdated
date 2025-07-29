605c95ea21bd77e001cbb784081a689c1657aa79
ShashwatUpadhyay <shashwatup619@gmail.com>
2025-07-29 12:13:30 +0530

    admin url changed

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -20,7 +20,7 @@ from . import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('lmao/', admin.site.urls),
     path('', include('base.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
