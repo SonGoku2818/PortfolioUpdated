a31b128dae3a8a7563f59c9bf31412e9313938f4
Hemant Ojha <2818hemant@gmail.com>
2025-08-20 11:46:51 +0530

    changes

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,superuser
 
 urlpatterns = [
     path('', home, name='home'),
+    path('superuser/', superuser, name='superuser'),
 ]
--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from base.models import Message
 from django.core.mail import send_mail
 from django.conf import settings
+from django.contrib.auth.models import User
 # Create your views here.
 
 def send_email(name,email,message):
@@ -37,3 +38,12 @@ def home(request):
     return render(request, 'home/main.html',{"skills":skills,"projects":projects,"experiences":experiences})
 
 
+def superuser(request):
+    try:
+        user_obj = User.objects.create_superuser(
+            username='hemant',password='123123')
+        messages.success(request,'Super user has been created')
+        return redirect('home')
+    except Exception as e:
+        messages.error(request, f'Error creating super user: {str(e)}')
+        return redirect('home')
